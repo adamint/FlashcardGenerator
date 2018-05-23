fccd77328d2bd1c09d83a14fef13c52e9c0d0b59
Adam Ratzman <adam@adamratzman.com>
2018-05-23 10:53:58 -0400

    document 2

--- src/main/java/com/adamratzman/flashcards/setup/FlashcardPreview.java
+++ src/main/java/com/adamratzman/flashcards/setup/FlashcardPreview.java
@@ -6,6 +6,15 @@ import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
 class FlashcardPreview {
+    /**
+     * Represents the preview pane of the current flashcard set, where user can delete or export flashcards.
+     * Given a preview, returns pane for a singular flashcard
+     *
+     * @param flashcard the flashcard to get preview for
+     * @param index the index in the flashcard list of this flashcard
+     * @param preview the Preview object to display in
+     * @return the preview pane for this singular flashcard
+     */
     static Pane getPreview(Flashcard flashcard, int index, Preview preview) {
         try {
             Pane root = FXMLLoader.load(preview.getClass().getResource("/flashcard-display-preview.fxml"));
@@ -14,6 +23,8 @@ class FlashcardPreview {
             TextField hintText = (TextField) root.lookup("#hint-text");
             ComboBox<String> answers = (ComboBox<String>) root.lookup("#answers");
             Hyperlink deleteQuestion = (Hyperlink) root.lookup("#delete");
+
+            // Ids must be unique so they are set to the question index. This allows for simple retrieval as well
 
             questionNumber.setText("Question " + (index + 1));
             questionNumber.setId("#question-" + index + "-number");
@@ -28,6 +39,8 @@ class FlashcardPreview {
             answers.getItems().addAll(flashcard.getAnswers());
 
             deleteQuestion.setId("#delete-" + index);
+
+            // Update text boxes where the flashcard size is included AND re-render the preview scene to reflect the deletion
             deleteQuestion.setOnMouseClicked(event -> {
                 preview.getFlashcards().removeIf(comp -> comp.getQuestion().equals(questionText.getText()));
                 preview.getFlashcardsLabel().setText("Total Flashcards (" + preview.getFlashcards().size() + ")");
