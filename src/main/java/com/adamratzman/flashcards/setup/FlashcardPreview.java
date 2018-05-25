40c7388b255bf6a5af6d32ad3d79b2cce3d4d229
Adam Ratzman <adam@adamratzman.com>
2018-05-25 10:27:36 -0400

    documentation

--- src/main/java/com/adamratzman/flashcards/setup/FlashcardPreview.java
+++ src/main/java/com/adamratzman/flashcards/setup/FlashcardPreview.java
@@ -4,43 +4,36 @@ import com.adamratzman.flashcards.models.Flashcard;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-
+/*
+ * When the preview object is called it creatas a new preview object specifically for a flashcard using the preview class.
+ * <br>
+ * It sets the question,hint, and deletes a question and canadd answers to a getAnswer list.
+ * <br>
+ * When the user clicks the deleteQuestion button the question will be deleted.
+ */
 class FlashcardPreview {
-    /**
-     * Represents the preview pane of the current flashcard set, where user can delete or export flashcards.
-     * Given a preview, returns pane for a singular flashcard
-     *
-     * @param flashcard the flashcard to get preview for
-     * @param index the index in the flashcard list of this flashcard
-     * @param preview the Preview object to display in
-     * @return the preview pane for this singular flashcard
-     */
     static Pane getPreview(Flashcard flashcard, int index, Preview preview) {
         try {
             Pane root = FXMLLoader.load(preview.getClass().getResource("/flashcard-display-preview.fxml"));
-            Label questionNumber = (Label) root.lookup("#question-number");
-            TextArea questionText = (TextArea) root.lookup("#question-text");
-            TextField hintText = (TextField) root.lookup("#hint-text");
-            ComboBox<String> answers = (ComboBox<String>) root.lookup("#answers");
-            Hyperlink deleteQuestion = (Hyperlink) root.lookup("#delete");
-
-            // Ids must be unique so they are set to the question index. This allows for simple retrieval as well
-
+            Label questionNumber = (Label) root.lookup("#question-number");//question the flashcard is on
+            TextArea questionText = (TextArea) root.lookup("#question-text");//question  number the flashcard is on
+            TextField hintText = (TextField) root.lookup("#hint-text");//hint the flashcard is on
+            ComboBox<String> answers = (ComboBox<String>) root.lookup("#answers");//answer the flashcard is on
+            Hyperlink deleteQuestion = (Hyperlink) root.lookup("#delete");//pushed to delete a card
+            //sets question number out of all
             questionNumber.setText("Question " + (index + 1));
             questionNumber.setId("#question-" + index + "-number");
-
+            //set to display the question the flashcard in on
             questionText.setText(flashcard.getQuestion());
             questionText.setId("#question-" + index + "-text");
-
+            //show the text to the card that is being previewed if there is one
             hintText.setText(flashcard.getHint() == null ? "There is no hint available" : flashcard.getHint());
             hintText.setId("#hint-" + index + "-text");
-
+            //shows the answer to the card that is being previewed
             answers.setId("#answers-" + index);
             answers.getItems().addAll(flashcard.getAnswers());
-
+            //used to delete a flashcard from the list
             deleteQuestion.setId("#delete-" + index);
-
-            // Update text boxes where the flashcard size is included AND re-render the preview scene to reflect the deletion
             deleteQuestion.setOnMouseClicked(event -> {
                 preview.getFlashcards().removeIf(comp -> comp.getQuestion().equals(questionText.getText()));
                 preview.getFlashcardsLabel().setText("Total Flashcards (" + preview.getFlashcards().size() + ")");
--- src/main/java/com/adamratzman/flashcards/setup/Preview.java
+++ src/main/java/com/adamratzman/flashcards/setup/Preview.java
@@ -18,6 +18,15 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
+/**
+ * This class gives the user a preview of their flashcard object they created.
+ * <br>
+ * It creates a new scene that contains a label (total-flashcards) with the total number of flashcards in the arrayList.
+ * <br>
+ * If the export button is clicked it will want a {@code file} for exporting.
+ * <br>
+ * This is used after the user has already created a set of flashcards and does not allow an empty list.
+ */
 public class Preview {
     private ArrayList<Flashcard> flashcards;
     private Stage stage = new Stage();
@@ -35,20 +44,24 @@ public class Preview {
             flashcardList = (ListView<Pane>) root.lookup("#flashcard-list");
             Scene scene = new Scene(root);
 
-            render();
-
+            render();//render the initial flashcards before any changes are made
+            //when the export hyperlink is pressed allow user to select where to export the deck
             scene.lookup("#export").setOnMouseClicked(event -> {
+                //check for flashcards to export if 0 export is not completed
                 if (flashcards.size() == 0)
                     Utils.showAlert(Alert.AlertType.ERROR, "You need at least one flashcard to export");
                 else {
+                    //let users select file or create their own
                     FileChooser exportFileChooser = new FileChooser();
                     exportFileChooser.getExtensionFilters()
                             .add(new FileChooser.ExtensionFilter("Flashcard Deck File (fdeck)", "*.fdeck"));
+                    //get the selected file
                     File chosen = exportFileChooser.showSaveDialog(stage);
+                    //if selected a file, export
                     if (chosen != null) export(chosen);
                 }
             });
-
+            //no individual flashcard should be selected
             scene.lookup("#preview").requestFocus();
             stage.setScene(scene);
             stage.show();
@@ -57,16 +70,23 @@ public class Preview {
         }
     }
 
+    /**
+     * This method exports the deck to the specified file.
+     * <br>
+     * @param file is the file to be exported to
+     */
     @SuppressWarnings("ResultOfMethodCallIgnored")
     private void export(File file) {
         try {
+            //file already exists delete it
             if (file.exists()) file.delete();
+            //create new file
             file.createNewFile();
-
+            //converts the Exporteddeck to a text using method in setup.java
             OutputStreamWriter outputWriter = new OutputStreamWriter(new FileOutputStream(file));
             outputWriter.write(setup.getGson().toJson(new ExportedDeck(System.currentTimeMillis(), flashcards)));
             outputWriter.close();
-
+            //shows if the deck was exported successfully
             Utils.showAlert(Alert.AlertType.INFORMATION, "Successfully exported flashcard deck to " + file.getPath());
         } catch (Exception e) {
             Utils.showAlert(Alert.AlertType.ERROR, "Unable to export flashcard deck here. Please check permissions");
@@ -85,6 +105,9 @@ public class Preview {
         return setup;
     }
 
+    /**
+     * This method renders the {@code flashcards} after a change is made by the user.
+     */
     void render() {
         flashcardList.getItems().clear();
         for (int i = 0; i < flashcards.size(); i++) {
